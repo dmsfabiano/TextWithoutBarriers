0f6eafe7d633b94ab6a91a79b146833aa3ac4f5a
dmsfabiano <dfabiano@mail.usf.edu>
2017-11-11 18:17:10 -0500

    chat almost works

--- app/src/main/java/com/hackriddle/textwithoutbarriers/Chat/Chat.java
+++ app/src/main/java/com/hackriddle/textwithoutbarriers/Chat/Chat.java
@@ -1,15 +1,110 @@
 package com.hackriddle.textwithoutbarriers.Chat;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.text.format.DateFormat;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hackriddle.textwithoutbarriers.R;
 
+
 public class Chat extends AppCompatActivity {
+
+    private static int SIGN_INT_REQUEST_CODE = 1;
+    private FirebaseListAdapter<ChatMessage> adapter;
+    RelativeLayout activity_main;
+    FloatingActionButton fab;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_sign_out)
+        {
+            // Sign them out
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_INT_REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Snackbar.make(activity_main, "Successfully signed in, welcome",Snackbar.LENGTH_SHORT).show();
+                displayChatMessage();
+            }
+            else
+            {
+                Snackbar.make(activity_main, "Could not sign in",Snackbar.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        activity_main = (RelativeLayout) findViewById(R.id.activity_chat);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(input.getText().toString(),
+                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+            input.setText("");
+            }
+        });
+
+        // Check if not sign in then get them out of here...
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+        {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_INT_REQUEST_CODE);
+        }
+        else{
+            Snackbar.make(activity_main, "Welcome " + FirebaseAuth.getInstance().getCurrentUser().getEmail(), Snackbar.LENGTH_SHORT).show();
+            displayChatMessage();
+        }
+    }
+
+    private void displayChatMessage() {
+        ListView listOfMessages = (ListView) findViewById(R.id.list_of_messages);
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,R.layout.list_item, FirebaseDatabase.getInstance().getReference())
+        {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                TextView messageText, messageUser, messageTime;
+                messageText = (TextView) v.findViewById(R.id.message_text);
+                messageUser = (TextView) v.findViewById(R.id.message_user);
+                messageTime = (TextView) v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-mm-yyyy (HH:mm:ss)", model.getMessageTime()));
+            }
+        };
     }
 }
