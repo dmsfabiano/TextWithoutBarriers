49ed60746a19f559e00752c9af21dbd13c655a92
dmsfabiano <dfabiano@mail.usf.edu>
2017-11-11 16:31:23 -0500

    chat message got better

---
+++ app/src/main/java/com/hackriddle/textwithoutbarriers/Chat/Chat.java
@@ -0,0 +1,15 @@
+package com.hackriddle.textwithoutbarriers.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackriddle.textwithoutbarriers.R;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
