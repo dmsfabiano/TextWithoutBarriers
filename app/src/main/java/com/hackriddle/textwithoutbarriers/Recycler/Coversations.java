f43665616526cd29c1545625ee27b78ec39b023b
david1437 <davidsherrier1997@gmail.com>
2017-11-11 14:09:52 -0500

    Recycler

---
+++ app/src/main/java/com/hackriddle/textwithoutbarriers/Recycler/Coversations.java
@@ -0,0 +1,17 @@
+package com.hackriddle.textwithoutbarriers.Recycler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.hackriddle.textwithoutbarriers.R;
+
+public class Coversations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversations);
+
+    }
+}
---
+++ app/src/main/java/com/hackriddle/textwithoutbarriers/Recycler/RecyclerData.java
@@ -0,0 +1,18 @@
+package com.hackriddle.textwithoutbarriers.Recycler;
+
+/**
+ * Created by david on 11/11/17.
+ */
+
+public class RecyclerData {
+    public String title;
+    public String msg;
+    public int imageId;
+
+    RecyclerData(String name, String msg, int imageId) {
+        this.title = title;
+        this.msg = msg;
+        this.imageId = imageId;
+    }
+
+}
