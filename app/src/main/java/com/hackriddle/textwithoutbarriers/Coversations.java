89bdb8054a4fb548f202f0369af54646fabbd1cd
dmsfabiano <dfabiano@mail.usf.edu>
2017-11-11 13:55:12 -0500

    Basic login & registration

--- app/src/main/java/com/hackriddle/textwithoutbarriers/Coversations.java
+++ app/src/main/java/com/hackriddle/textwithoutbarriers/Coversations.java
@@ -6,10 +6,6 @@ import android.widget.TextView;
 
 public class Coversations extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +14,7 @@ public class Coversations extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText("Welcome to Text without Borders! ");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
